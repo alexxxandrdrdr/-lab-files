8375ca13e903dbd9a3bb55d6b7fcd2641667736d
alexxxandrdrdr <110187179+alexxxandrdrdr@users.noreply.github.com>
2022-07-30 16:06:15 +0300

    C# labs
    C#labs

---
+++ Lab1/Ex1/Starter/Lab1.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;        
+            while ((line = Console.ReadLine()) != null)
+            {
+                line = line.Replace(",", " y:");
+                line = "x:" + line;
+                Console.WriteLine(line);
+            }
+
+        }
+    }
+}
+
